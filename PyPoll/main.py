51aaed2de14df033fe14d1c615a38abbff45300b
Chacha <alainshamir@gmail.com>
2018-06-14 23:02:50 -0400

    Here is my Unit 3 Assignment

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -54,39 +54,41 @@ with open(csvpath) as election_data:
             candidate_votes[candidate_name] = 0
 
         # Then add a vote to that candidate's count
-        candidate_votes[candidate_name] = candidate_votes[candidate_name] + 1
-
+            candidate_votes[candidate_name] = candidate_votes[candidate_name] + 1
+        else:
+            candidate_votes[candidate_name] = candidate_votes[candidate_name] + 1
 # Print the results and export the data to our text file
 with open(output_file, "w") as txt_file:
+    output_1 = (
+    f"\nElection Results\n"
+    f"----------------------------\n"
+    f"The total number of votes cast: {total_votes}\n")
+    txt_file.write(output_1)
 
     # Determine the winner by looping through the counts
-    for candidate in candidate_votes:
+    
 
+    for candidate in candidate_votes:
+       
         # Retrieve vote count and percentage
-        votes = candidate_votes.get(candidate)
-        vote_percentage = float(votes) / float(total_votes) * 100
+        votes = candidate_votes[candidate]
+        
+        vote_percentage = (votes /total_votes) * 100
 
         # Determine winning vote count and candidate
         if (votes > winning_count):
             winning_count = votes
             winning_candidate = candidate
+    
+# Print each candidate's voter count and percentage (to terminal)
+        output_2 = f"{candidate}: {vote_percentage:.3f}% ({votes})\n"
+        txt_file.write(output_2)
 
-        # Generate Output Summary
-output_1 = (
-    f"\nElection Results\n"
-    f"----------------------------\n"
-    f"The total number of votes cast: {total_votes}\n"
-    f"A complete list of candidates who received votes: {candidate_votes}\n"
-    f"Each Candidate voter percentage and counts {candidate}: {vote_percentage:.3f}% ({votes})\n"
-    f"The total number of votes each candidate won: {votes}\n"
+    output_3=(
     f"The winner of the election based on popular vote: {winning_candidate}\n")
 
 # Print the output (to terminal)
-print(output_1)
-
-# Export the results to text file
-with open(output_file, "w") as txt_file:
-    txt_file.write(output_1)
+    txt_file.write(output_3)
 
 # Python Homework PyPoll
 #In this challenge, you are tasked with helping a small, rural town modernize its vote-counting process. (Up until now, Uncle Cleetus had been trustfully tallying them one-by-one, but unfortunately, his concentration isn't what it used to be.)
@@ -102,13 +104,13 @@ import csv
 import os
 
 # Set path for file
-csvpath_2 = os.path.join("C:\\Users\\Alain\\GWDC201805DATA3-Class-Repository-DATA\\Homework\\03-Python\\Instructions\\PyPoll\\raw_data\\", "election_data_2.csv")
+csvpath = os.path.join("C:\\Users\\Alain\\GWDC201805DATA3-Class-Repository-DATA\\Homework\\03-Python\\Instructions\\PyPoll\\raw_data\\", "election_data_2.csv")
 
 # Create output file name
-output_file_2 = "C:/Users/Alain/GWDC201805DATA3-Class-Repository-DATA/Unit_3_Assignment/python-challenge/PyPoll/election_data_2.txt"  
+output_file = "C:/Users/Alain/GWDC201805DATA3-Class-Repository-DATA/Unit_3_Assignment/python-challenge/PyPoll/election_data_2.txt"  
 
 
-# Total Vote Starting point
+# Total Vote Counter
 total_votes = 0
 
 # Candidate Options and Vote Counters
@@ -120,8 +122,8 @@ winning_candidate = ""
 winning_count = 0
 
 # Read the csv and convert it into a list of dictionaries
-with open(csvpath_2) as election_data_2:
-    reader = csv.DictReader(election_data_2)
+with open(csvpath) as election_data:
+    reader = csv.DictReader(election_data)
 
     # For each row...
     for row in reader:
@@ -144,36 +146,39 @@ with open(csvpath_2) as election_data_2:
             candidate_votes[candidate_name] = 0
 
         # Then add a vote to that candidate's count
-        candidate_votes[candidate_name] = candidate_votes[candidate_name] + 1
-
+            candidate_votes[candidate_name] = candidate_votes[candidate_name] + 1
+        else:
+            candidate_votes[candidate_name] = candidate_votes[candidate_name] + 1
 # Print the results and export the data to our text file
-with open(output_file_2, "w") as txt_file:
+with open(output_file, "w") as txt_file:
+    output_1 = (
+    f"\nElection Results\n"
+    f"----------------------------\n"
+    f"The total number of votes cast: {total_votes}\n")
+    txt_file.write(output_1)
 
     # Determine the winner by looping through the counts
-    for candidate in candidate_votes:
+    
 
+    for candidate in candidate_votes:
+       
         # Retrieve vote count and percentage
-        votes = candidate_votes.get(candidate)
-        vote_percentage = float(votes) / float(total_votes) * 100
+        votes = candidate_votes[candidate]
+        
+        vote_percentage = (votes /total_votes) * 100
 
         # Determine winning vote count and candidate
         if (votes > winning_count):
             winning_count = votes
             winning_candidate = candidate
+    
+# Print each candidate's voter count and percentage (to terminal)
+        output_2 = f"{candidate}: {vote_percentage:.3f}% ({votes})\n"
+        txt_file.write(output_2)
 
-        # Generate Output Summary
-output_2 = (
-    f"\nElection Results\n"
-    f"----------------------------\n"
-    f"The total number of votes cast: {total_votes}\n"
-    f"A complete list of candidates who received votes: {candidate_votes}\n"
-    f"Each Candidate voter percentage and counts {candidate}: {vote_percentage:.3f}% ({votes})\n"
-    f"The total number of votes each candidate won: {votes}\n"
+    output_3=(
     f"The winner of the election based on popular vote: {winning_candidate}\n")
 
 # Print the output (to terminal)
-print(output_2)
+    txt_file.write(output_3)
 
-# Export the results to text file
-with open(output_file_2, "w") as txt_file:
-    txt_file.write(output_2)
